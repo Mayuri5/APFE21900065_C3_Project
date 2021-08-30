da8a870abfc4eb0665625be7a13b5540e5c9ac6d
Mayuri Adhikar <mayuriadhikar0@gmail.com>
2021-08-30 20:51:40 +0530

    Part 3: Solution

--- Restaurant_project -Student version/src/main/java/Restaurant.java
+++ Restaurant_project -Student version/src/main/java/Restaurant.java
@@ -25,25 +25,27 @@ public class Restaurant {
         return false;
     }
 
-    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+    public LocalTime getCurrentTime() {
+        return LocalTime.now();
+    }
 
     public List<Item> getMenu() {
         return this.menu;
     }
 
-    private Item findItemByName(String itemName){
-        for(Item item: menu) {
-            if(item.getName().equals(itemName))
+    private Item findItemByName(String itemName) {
+        for (Item item : menu) {
+            if (item.getName().equals(itemName))
                 return item;
         }
         return null;
     }
 
     public void addToMenu(String name, int price) {
-        Item newItem = new Item(name,price);
+        Item newItem = new Item(name, price);
         menu.add(newItem);
     }
-    
+
     public void removeFromMenu(String itemName) throws itemNotFoundException {
 
         Item itemToBeRemoved = findItemByName(itemName);
@@ -52,12 +54,13 @@ public class Restaurant {
 
         menu.remove(itemToBeRemoved);
     }
-    public void displayDetails(){
-        System.out.println("Restaurant:"+ name + "\n"
-                +"Location:"+ location + "\n"
-                +"Opening time:"+ openingTime +"\n"
-                +"Closing time:"+ closingTime +"\n"
-                +"Menu:"+"\n"+getMenu());
+
+    public void displayDetails() {
+        System.out.println("Restaurant:" + name + "\n"
+                + "Location:" + location + "\n"
+                + "Opening time:" + openingTime + "\n"
+                + "Closing time:" + closingTime + "\n"
+                + "Menu:" + "\n" + getMenu());
 
     }
 
@@ -65,4 +68,12 @@ public class Restaurant {
         return name;
     }
 
+    public double getOrderTotal(List<String> items) {
+        double price = 0;
+        for (String itemName : items) {
+            Item item = findItemByName(itemName);
+            price += item.getPrice();
+        }
+        return price;
+    }
 }
